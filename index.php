a84a13fbad3c26602bad63b823082898bef68392
roberto <robertoramirezmoreno@gmail.com>
2023-04-18 20:20:45 +0200

    primeros ejercicios

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+//1. Crea programas de operaciones con dos variables (suma, resta, etc).
+
+$varDos = "1";
+
+$varTres = "4";
+
+$varCinco = $varDos + $varTres;
+
+var_dump($varCinco);
+
+$varSeis = $varTres - $varDos;
+
+var_dump($varSeis);
+
+$varSiete = $varSeis * 2;
+
+var_dump($varSiete);
+
+//2. Crear array e imprimir un elemento del array.
+
+
+$varArray = [3,4,6,8,10];
+
+$varArraySecondElement = $varArray[1];
+
+var_dump($varArraySecondElement);
+
+
+
+
+
+
+
+?> 
