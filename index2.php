38db0056413a8808b5420f98ccba3765841dcf19
roberto <robertoramirezmoreno@gmail.com>
2023-04-21 18:32:13 +0200

    falta 8

--- index2.php
+++ index2.php
@@ -1,19 +1,7 @@
 <?php
 
-//10. Crear una función que dado un número x imprima solo los valores impares.
-function obtenerNumerosAleatorios($maximo) {
-    $numeros = array();
-    while (count($numeros) < 5) {
-        $numero_aleatorio = rand(1, $maximo);
-        if (!in_array($numero_aleatorio, $numeros)) {
-            $numeros[] = $numero_aleatorio;
-        }
-    }
-    return $numeros;
-}
 
-$numeros_aleatorios = obtenerNumerosAleatorios(25);
-var_dump($numeros_aleatorios);
+
 
 
 
