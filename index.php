215c33fe349e1f8ab1ad698c5dd820053ac3979b
roberto <robertoramirezmoreno@gmail.com>
2023-04-24 00:58:57 +0200

    till 10

--- index.php
+++ index.php
@@ -5,28 +5,28 @@ $varDos = 1;
 $varTres = 4;
 
 $varCinco = $varDos + $varTres;
-var_dump($varCinco);
+//var_dump($varCinco);
 
 $varSeis = $varTres - $varDos;
-var_dump($varSeis);
+//var_dump($varSeis);
 
 $varSiete = $varSeis * 2;
-var_dump($varSiete);
+//var_dump($varSiete);
 
 // Ejercicio 2: Crear array e imprimir un elemento del array.
 $varArray = [3, 4, 6, 8, 10];
 $varArraySecondElement = $varArray[1];
-var_dump($varArraySecondElement);
+//var_dump($varArraySecondElement);
 
 // Ejercicio 3: Crear un array de arrays e imprimir un elemento del array.
 $array_arrays = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]];
-var_dump($array_arrays[1]);
-var_dump($array_arrays[1][1]);
+//var_dump($array_arrays[1]);
+//var_dump($array_arrays[1][1]);
 
 // Ejercicio 4: Crear un programa que imprima los primeros diez números.
 $num = 1;
 while ($num <= 10) {
-  echo "voy por el número " . $num . "\n";
+  //echo "voy por el número " . $num . "\n";
   $num++;
 }
 
@@ -35,19 +35,17 @@ $sumaFirstTen = 0;
 for ($i = 1; $i <= 10; $i++) {
   $sumaFirstTen += $i;
 }
-echo 'La suma de los primeros diez números es ' . $sumaFirstTen . "\n";
+//echo 'La suma de los primeros diez números es ' . $sumaFirstTen . "\n";
 
 // Ejercicio 6: Crear un programa que imprima X números random.
 for ($i = 0; $i < 5; $i++) {
   $numero_aleatorio = rand(1, 100);
-  echo $numero_aleatorio . "\n\n";
+  //echo $numero_aleatorio . "\n\n";
 }
-
 for ($i = 0; $i < 5; $i++) {
   $randomNum = rand(500, 1000);
-  echo $randomNum . "\n";
+  //echo $randomNum . "\n";
 }
-
 // Ejercicio 7: Encapsular los ejercicios anteriores en funciones.
 
 // Ejercicio 7.5: Función para sumar los primeros diez números.
@@ -58,9 +56,8 @@ function sumarPrimerosDiezNumeros() {
   }
   return $suma1;
 }
-
 $miSumaDiezPrimeros = sumarPrimerosDiezNumeros();
-echo "El resultado de sumar los diez primeros números es: $miSumaDiezPrimeros\n";
+//echo "El resultado de sumar los diez primeros números es: $miSumaDiezPrimeros\n";
 
 // Ejercicio 7.6: Función para imprimir X números random.
 function cincoNumerosRandom() {
@@ -71,15 +68,17 @@ function cincoNumerosRandom() {
     }
     return $numeros;
   }
-  
-  $numRamdom = cincoNumerosRandom();
-  echo ("aquí tienes tus números aleatorios ");
-  var_dump($numRamdom);
+  //$numRamdom = cincoNumerosRandom();
+  //echo ("aquí tienes tus números aleatorios ");
+  //var_dump($numRamdom);
 
-//8. Crear una función que imprima X valores random en el
-intervarlo //0 - X. (FALTA)
-
-
+//8. Crear una función que imprima X valores random en el intervalo//0 - X.
+function print_random_numbers($count) {
+  for ($i = 0; $i < $count; $i++) {
+    echo rand(0, $count) . " ";
+  }
+}
+//print_random_numbers(10);
 //9. Crear una función que dado un número devuelva solo los valores pares.//.
 $numero = 0;
 function obtenerPares($numero) {
@@ -91,23 +90,27 @@ function obtenerPares($numero) {
     }
     return $pares;
 }
-
-$misPares = obtenerPares(102);
-var_dump($misPares);
-
+//$misPares = obtenerPares(102);
+//var_dump($misPares);
 //10. Crear una función que dado un número x imprima solo los valores impares.
-$numero = 0;
-function obtenerImpares($numero){
-  $impar = array();
-  for ($i = 1; $i <= $numero; $i++){
-      if ($i % 2 != 0) {
-          $impar[] = $i;
-      }
+function obtenerImpares($numero) {
+  $impares = array(); 
+  for ($i = 1; $i <= $numero; $i++) {
+    if ($i % 2 != 0) {
+      $impares[] = $i;
+    }
   }
-  return $impar;   
+  return $impares;
 }
-$misImpares = obtenerImpares(50);
-var_dump($misImpares);
+
+$impares = obtenerImpares(10);
+
+foreach ($impares as $numero) {
+  echo $numero . " ";
+}
+
+
+
 
 ?>
 
--- index2.php
+++ index2.php
@@ -1,6 +1,7 @@
 <?php
-
-
+//PHP 21/04
+//métodos. lo más importante es "extends". No hay, herencia múltiple pero puede simularse con PHP.
+// 
 
 
 
